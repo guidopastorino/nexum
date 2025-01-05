19f818e7632cb73964181c193910501f6ccb574a
Guido Pastorino <guidopasto05@gmail.com>
2025-01-05 18:39:11 -0300

    Adding drawerOverlay class for post clicking prevention

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -30,7 +30,7 @@ const BottomSheet = ({ trigger, children, isOpen, setOpen }: BottomSheetProps) =
         {trigger}
       </Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-black/60 z-50" onClick={() => toggleOpen(false)} />
+        <Drawer.Overlay className="drawerOverlay fixed inset-0 bg-black/60 z-50" onClick={() => toggleOpen(false)} />
         <Drawer.Content className="dark:bg-neutral-900 bg-white text-black dark:text-white h-fit fixed bottom-0 left-0 right-0 outline-none z-50 pt-1 rounded-t-lg">
           <div className="w-full p-2 flex justify-center items-center">
             <span className='dark:bg-neutral-700 bg-gray-300 w-full max-w-10 rounded-full h-[5px]'></span>
