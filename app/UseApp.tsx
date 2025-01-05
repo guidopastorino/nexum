d04ea9a001f583c55366117c1bda30789d3741a7
Guido Pastorino <guidopasto05@gmail.com>
2025-01-05 18:35:33 -0300

    Removing useEffect

--- app/UseApp.tsx
+++ app/UseApp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { store } from "@/store/index";
