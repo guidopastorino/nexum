91f3fcc2e036ca44b707fc4040f0d57e26d94ad1
Guido Pastorino <guidopasto05@gmail.com>
2025-01-08 01:02:46 -0300

    adding filter verif. when fetching post data

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -22,8 +22,10 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
 
     const { id } = params;
 
+    const filter = isValidObjectId(id) ? { _id: id } : { maskedId: id }
+
     const post = await Post
-      .findOne({ maskedId: id })
+      .findOne(filter)
       .populate(postPopulateOptions);
 
     if (!post) {
@@ -45,77 +47,5 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
   } catch (error) {
     console.error("Error:", error);
     return NextResponse.json({ message: "Internal server error" }, { status: 500 });
-  }
-}
-
-// ────────────────────────────────────────────────────────────────────────────────────────────────────
-// ────────────────────────────────────────────────────────────────────────────────────────────────────
-
-
-// Ruta: /api/posts/[id] (DELETE para manejar todos los tipos de posts)
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-  const userId = session.user.id;
-  await dbConnect();
-
-  const { id } = params;
-  const filter = isValidObjectId(id) ? { _id: id } : { maskedId: id };
-  const postToDelete = await Post.findOne(filter);
-
-  if (!postToDelete) {
-    return NextResponse.json({ error: "Post not found" }, { status: 404 });
-  }
-
-  if (postToDelete.type === 'normal') {
-    // Eliminar posts que repostean este post (posts de tipo 'repost' que su array 'repostedFrom' tienen el id de este post)
-    await Post.deleteMany({ type: 'repost', repostedFrom: postToDelete._id });
-  } else if (postToDelete.type === 'repost') {
-    if (postToDelete.creator.toString() === userId) {
-      // Eliminar el post reposteado completo
-      await handleDeleteRepost(postToDelete.repostedFrom.toString(), userId, postToDelete._id.toString());
-    } else {
-      // Deshacer repost sin eliminar el post actual
-      await handleUndoRepost(postToDelete.repostedFrom.toString(), userId);
-    }
-  } else if (postToDelete.type === 'quote') {
-    await handleRemoveQuote(postToDelete.quotedPost.toString(), postToDelete._id.toString());
-  }
-
-  await Post.deleteOne({ _id: postToDelete._id });
-
-  // Reorganizar los posts del usuario, eliminando el post eliminado del array 'posts'
-  const user = await User.findById(userId);
-  if (user) {
-    user.posts = user.posts.filter((postId: Types.ObjectId) => postId.toString() !== postToDelete._id.toString());
-    await user.save();
-  }
-
-  return NextResponse.json({ message: "Post deleted successfully" }, { status: 200 });
-}
-
-async function handleDeleteRepost(repostedFromId: string, userId: string, repostId: string) {
-  const repostedPost = await Post.findById(repostedFromId);
-  if (repostedPost) {
-    repostedPost.reposts = repostedPost.reposts.filter((uid: Types.ObjectId) => uid.toString() !== userId);
-    await repostedPost.save();
-  }
-}
-
-async function handleUndoRepost(repostedFromId: string, userId: string) {
-  const repostedPost = await Post.findById(repostedFromId);
-  if (repostedPost) {
-    repostedPost.reposts = repostedPost.reposts.filter((uid: Types.ObjectId) => uid.toString() !== userId);
-    await repostedPost.save();
-  }
-}
-
-async function handleRemoveQuote(quotedPostId: string, quoteId: string) {
-  const quotedPost = await Post.findById(quotedPostId);
-  if (quotedPost) {
-    quotedPost.quotes = quotedPost.quotes.filter((qid: Types.ObjectId) => qid.toString() !== quoteId);
-    await quotedPost.save();
   }
 }
