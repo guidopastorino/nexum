3426e86f3f954260214dc7dac374f6cdd65bd0ee
Guido Pastorino <guidopasto05@gmail.com>
2025-01-10 16:13:45 -0300

    adding update profile modal

--- components/buttons/UserProfileButtons.tsx
+++ components/buttons/UserProfileButtons.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { HiPencil } from "react-icons/hi2";
-import { BsThreeDots } from 'react-icons/bs';
+import { BsThreeDots, BsTrash } from 'react-icons/bs';
 import { IoSearchOutline } from 'react-icons/io5';
 import { useSession } from 'next-auth/react';
 import UnfollowUserButton from '@/components/buttons/post/UnfollowUserButton';
@@ -11,10 +10,20 @@ import FollowUserButton from '@/components/buttons/post/FollowUserButton';
 import { useFollowUser } from '@/hooks/useFollowUser';
 import useToast from '@/hooks/useToast';
 import AuthModal from '@/components/modal/AuthModal';
-import LoggedOut from '../auth/LoggedOut';
-import LoggedIn from '../auth/LoggedIn';
 import ResponsiveMenu from '../ResponsiveMenu';
-import { GuestProfileMenu, OthersProfileMenu } from '../ProfileOptionsMenu';
+import { OthersProfileMenu } from '../ProfileOptionsMenu';
+import Modal from '../modal/Modal';
+import useUser from '@/hooks/useUser';
+import { FaCamera } from 'react-icons/fa6';
+import { updateUser } from '@/utils/fetchFunctions';
+import { uploadFiles } from '@/actions/uploadFiles';
+import { UpdateUserData, UserState } from '@/types/types';
+import { useQueryClient } from 'react-query';
+import useAuthStateListener from '@/hooks/useAuthStateListener';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@/store/user/userSlice';
+import { utapi } from "@/server/uploadthing";
+import { resizeImage } from '@/utils/resizeImage';
 
 type UserProfileButtonsProps = {
   disableIfSameUser?: boolean;
@@ -35,9 +44,32 @@ const UserProfileButtons: React.FC<UserProfileButtonsProps> = ({
   isFollowedByUser,
   postId
 }) => {
+  const { data: session } = useSession();
+  const user = useUser()
+
+  const dispatch = useDispatch();
+
+  const queryClient = useQueryClient()
+
+  const ProfileImageInputRef = useRef<HTMLInputElement | null>(null)
+  const BannerImageInputRef = useRef<HTMLInputElement | null>(null)
+
+  const [loading, setIsLoading] = useState<boolean>(false)
+  const [userUpdatedData, setUserUpdatedData] = useState({
+    fullname: '',
+    description: '',
+    profileImage: null as File | null,
+    bannerImage: null as File | null,
+  })
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  useEffect(() => {
+    console.log(userUpdatedData)
+    setIsEditing(!!userUpdatedData.fullname || !!userUpdatedData.description || !!userUpdatedData.profileImage || !!userUpdatedData.bannerImage)
+  }, [userUpdatedData])
+
   const [initialFollowState, setInitialFollowState] = useState<boolean>(isFollowingUser || false)
 
-  const { data: session } = useSession();
   const [isFollowing, setIsFollowing] = useState(isFollowingUser);
   const mutation = useFollowUser(userId, isFollowing, postId);
   const { showToast } = useToast();
@@ -51,6 +83,135 @@ const UserProfileButtons: React.FC<UserProfileButtonsProps> = ({
     setIsFollowing(updatedFollowing);
     showToast(updatedFollowing ? `Followed @${username}` : `Unfollowed @${username}`);
     mutation.mutate();
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setUserUpdatedData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleMediaFilesChange = async (event: React.ChangeEvent<HTMLInputElement>, type: 'profileImage' | 'bannerImage') => {
+    if (!event.target.files?.length) return;
+
+    const file = event.target.files[0];
+
+    try {
+      const width: number = type == 'profileImage' ? 400 : 1080
+      const height: number = type == 'profileImage' ? 400 : 360
+      const resizedFile = await resizeImage(file, width, height);
+      setUserUpdatedData((prev) => ({
+        ...prev,
+        [type]: resizedFile,  // Usar la imagen redimensionada
+      }));
+    } catch (error) {
+      console.error('Error resizing image:', error);
+    }
+  };
+
+  const updateProfile = async () => {
+    setIsLoading(true);
+    try {
+      // Inicializar las variables para las URLs de las imágenes
+      let profileImageUrl = '';
+      let bannerImageUrl = '';
+
+      // Creamos una sola instancia de FormData
+      const formData = new FormData();
+
+      // Solo agregamos las imágenes que realmente existen (no nulas ni vacías)
+      if (userUpdatedData.profileImage) {
+        formData.append('files', userUpdatedData.profileImage);
+      }
+
+      if (userUpdatedData.bannerImage) {
+        formData.append('files', userUpdatedData.bannerImage);
+      }
+
+      // Si se seleccionó al menos una imagen, proceder con la subida
+      // [0]: profileImage
+      // [1]: bannerImage
+      if (formData.has('files')) {
+        const res = await uploadFiles(formData);
+
+        // Asignar URLs solo si las imágenes existen
+        if (res[0] && res[0].data?.url) {
+          profileImageUrl = res[0].data.url; // Asignar URL de la imagen de perfil
+        }
+
+        if (res[1] && res[1].data?.url) {
+          bannerImageUrl = res[1].data.url; // Asignar URL de la imagen de banner
+        }
+      }
+
+      // Actualizamos los datos de usuario (solo asignando lo que se ha cambiado)
+      const data: Partial<UserState> = {
+        fullname: userUpdatedData.fullname || undefined,
+        description: userUpdatedData.description || undefined,
+        profileImage: profileImageUrl || undefined,
+        bannerImage: bannerImageUrl || undefined,
+      };
+
+      // Agregar los campos solo si tienen un valor
+      if (userUpdatedData.fullname) {
+        data.fullname = userUpdatedData.fullname;
+      }
+
+      if (userUpdatedData.description) {
+        data.description = userUpdatedData.description;
+      }
+
+      if (profileImageUrl) {
+        data.profileImage = profileImageUrl;
+      }
+
+      if (bannerImageUrl) {
+        data.bannerImage = bannerImageUrl;
+      }
+
+      const response = await updateUser(session?.user.id!, data as UpdateUserData);
+
+      showToast(response.message)
+
+      // invalidar queries
+      queryClient.invalidateQueries(['userProfile', user.username])
+      queryClient.invalidateQueries(['creatorDataHoverCard', session?.user.id])
+      queryClient.invalidateQueries(['userPosts', user.username])
+
+      // Actualiza el estado de Redux solo con los cambios
+      dispatch(setUser({
+        ...user,  // Conserva las propiedades existentes para evitar undefined
+        fullname: data.fullname ?? user.fullname,
+        description: data.description ?? user.description,
+        profileImage: profileImageUrl || user.profileImage,
+        bannerImage: bannerImageUrl || user.bannerImage,
+      }));
+
+      // eliminar imagenes de perfil y banner que usaba el usuario antes
+      // deleteFiles takes in a fileKey
+      // eliminar su imagen de perfil anterior (si seleccionó una nueva)
+      // if (profileImageUrl && user.profileImage) {
+      //   await utapi.deleteFiles(user.profileImage)
+      // }
+      // eliminar su imagen de banner anterior (si seleccionó una nueva)
+      // if (bannerImageUrl && user.bannerImage) {
+      //   await utapi.deleteFiles(user.bannerImage)
+      // }
+
+      setUserUpdatedData({
+        fullname: '',
+        description: '',
+        profileImage: null,
+        bannerImage: null,
+      })
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      showToast(error instanceof Error ? error.message : "Error trying to upload profile")
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   // Si no hay sesión, renderiza AuthModal o nada según disableIfSameUser
@@ -74,13 +235,91 @@ const UserProfileButtons: React.FC<UserProfileButtonsProps> = ({
   // Si el usuario logueado es el mismo, renderiza "Editar perfil"
   if (session.user.id === userId) {
     return (
-      <Link
-        href={`/settings`}
-        className="h-9 rounded-full py-3 px-4 gap-3 font-medium flex justify-center items-center itemHover border border-gray-200 dark:border-neutral-700/70"
+      <Modal
+        buttonTrigger={
+          <div
+            className="h-9 rounded-full py-3 px-4 gap-3 font-medium flex justify-center items-center itemHover border border-gray-200 dark:border-neutral-700/70"
+          >
+            <HiPencil />
+            <span>Editar perfil</span>
+          </div>
+        }
+        width={500}
       >
-        <HiPencil />
-        <span>Editar perfil</span>
-      </Link>
+        <div className="bg-white dark:bg-neutral-800 p-3">
+          {/*  */}
+          <input
+            ref={ProfileImageInputRef}
+            type="file"
+            accept="image/*"
+            onChange={(event) => handleMediaFilesChange(event, 'profileImage')}
+            hidden
+          />
+          <input
+            ref={BannerImageInputRef}
+            type="file"
+            accept="image/*"
+            onChange={(event) => handleMediaFilesChange(event, 'bannerImage')}
+            hidden
+          />
+          {/*  */}
+          <div className="relative w-full">
+            {/* banner */}
+            <div className='h-[150px]'>
+              <img
+                src={userUpdatedData.bannerImage ? URL.createObjectURL(userUpdatedData.bannerImage) : (user.bannerImage || "https://www.solidbackgrounds.com/images/1584x396/1584x396-light-sky-blue-solid-color-background.jpg")}
+                className="w-full h-full object-cover"
+              />
+              <button onClick={() => BannerImageInputRef.current?.click()} className='hover:brightness-90 absolute top-2 right-2 w-7 h-7 text-xs rounded-full flex justify-center items-center text-white bg-black cursor-pointer'><FaCamera /></button>
+            </div>
+            {/* user info */}
+            <div className='flex flex-col justify-center items-stretch gap-2 p-4 mt-14'>
+              {/* fullname */}
+              <p className="font-medium pt-2">Fullname</p>
+              <input
+                type="text"
+                name="fullname"
+                className="formInput"
+                value={userUpdatedData.fullname} // Usar el estado aquí
+                onChange={handleInputChange}
+              />
+              {/* description */}
+              <p className="font-medium pt-2">Description</p>
+              <textarea
+                name="description"
+                className="formInput"
+                value={userUpdatedData.description || ""} // Usar el estado aquí
+                onChange={handleInputChange}
+                placeholder="Description"
+              />
+            </div>
+            {/* profile image */}
+            <div className='top-28 left-3 absolute z-20'>
+              <div className="relative w-20 h-20 md:w-28 md:h-28 rounded-full overflow-hidden shadow-lg">
+                <img
+                  src={userUpdatedData.profileImage ? URL.createObjectURL(userUpdatedData.profileImage) : (user.profileImage || "/default_pfp.jpg")}
+                  className="w-full h-full object-cover object-center cursor-pointer hover:brightness-90 duration-100"
+                />
+              </div>
+              <button onClick={() => ProfileImageInputRef.current?.click()} className='hover:brightness-90 absolute top-2 right-2 w-7 h-7 text-xs rounded-full flex justify-center items-center text-white bg-black cursor-pointer'><FaCamera /></button>
+            </div>
+          </div>
+
+          <div className="w-full mt-2 flex justify-end items-center gap-2">
+            {isEditing && <button onClick={() => setUserUpdatedData({
+              fullname: '',
+              description: '',
+              profileImage: null,
+              bannerImage: null,
+            })} className='w-9 h-9 flex justify-center items-center rounded-full text-red-700 border borderColor itemHover'>
+              <BsTrash />
+            </button>}
+            <button disabled={loading} onClick={updateProfile} className={`px-4 h-9 text-white bg-orange-600 rounded-full text-sm font-medium hover:brightness-90 active:brightness-75 duration-100`}>
+              {loading ? "Saving changes..." : "Save changes"}
+            </button>
+          </div>
+        </div>
+      </Modal>
     );
   }
 
