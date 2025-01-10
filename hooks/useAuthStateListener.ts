6079f512e4949f92a10f6d1d6aab7654cfad3810
Guido Pastorino <guidopasto05@gmail.com>
2025-01-10 16:13:59 -0300

    adding description as first state for user

--- hooks/useAuthStateListener.ts
+++ hooks/useAuthStateListener.ts
@@ -21,6 +21,7 @@ const useAuthStateListener = () => {
             _id: userData._id,
             fullname: userData.fullname,
             username: userData.username,
+            description: userData.description,
             isVerified: userData.isVerified,
             email: userData.email,
             profileImage: userData.profileImage,
