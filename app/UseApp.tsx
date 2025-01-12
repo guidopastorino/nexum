a5cbab235e9d25b289b5e6a3a727ee162cf71b89
Guido Pastorino <guidopasto05@gmail.com>
2025-01-12 14:42:52 -0300

    Refactoring UseApp inorder to show the modal

--- app/UseApp.tsx
+++ app/UseApp.tsx
@@ -3,49 +3,64 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import { store } from "@/store/index";
+import { RootState, store } from "@/store/index";
 import useAuthStateListener from "@/hooks/useAuthStateListener";
 import { QueryClient, QueryClientProvider } from "react-query";
 import NextTopLoader from 'nextjs-toploader';
 import { ThemeProvider } from "next-themes";
 import Toast from '@/components/Toast';
+import { useSelector } from "react-redux";
+import Modal from "@/components/modal/Modal";
 
 const queryClient = new QueryClient();
 
-const UseApp = ({ children }: { children: React.ReactNode }) => {
+export default function UseApp({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <AppWrapper>{children}</AppWrapper>
+    </Provider>
+  );
+};
+
+const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { isOpen, content } = useSelector((state: RootState) => state.modal);
+
   return (
     <SessionProvider>
       <ThemeProvider
-        storageKey='theme'
-        defaultTheme='system'
+        storageKey="theme"
+        defaultTheme="system"
         enableSystem
         enableColorScheme
         disableTransitionOnChange
         attribute="class"
       >
-        <Provider store={store}>
-          <AuthStateListenerWrapper>
-            <QueryClientProvider client={queryClient}>
-              <NextTopLoader
-                color="#ea580c"
-                initialPosition={0.08}
-                crawlSpeed={200}
-                height={3}
-                crawl={true}
-                showSpinner={false}
-                easing="ease"
-                speed={200}
-                shadow="0 0 10px #2299DD,0 0 5px #2299DD"
-                template='<div class="bar" role="bar"><div class="peg"></div></div> 
+        <AuthStateListenerWrapper>
+          <QueryClientProvider client={queryClient}>
+            <NextTopLoader
+              color="#ea580c"
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={3}
+              crawl={true}
+              showSpinner={false}
+              easing="ease"
+              speed={200}
+              shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+              template='<div class="bar" role="bar"><div class="peg"></div></div> 
   <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-                zIndex={1600}
-                showAtBottom={false}
-              />
-              <Toast />
-              {children}
-            </QueryClientProvider>
-          </AuthStateListenerWrapper>
-        </Provider>
+              zIndex={1600}
+              showAtBottom={false}
+            />
+            {/* toast */}
+            <Toast />
+            {/* modal */}
+            <Modal modalId="globalModal" isOpen={isOpen} width="500px">
+              {content}
+            </Modal>
+            {children}
+          </QueryClientProvider>
+        </AuthStateListenerWrapper>
       </ThemeProvider>
     </SessionProvider>
   );
@@ -56,5 +71,3 @@ const AuthStateListenerWrapper = ({ children }: { children: React.ReactNode }) =
   useAuthStateListener();
   return <>{children}</>;
 };
-
-export default UseApp;
