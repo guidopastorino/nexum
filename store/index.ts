9d5dd625e822f14c33a56633353e5555406b29dc
Guido Pastorino <guidopasto05@gmail.com>
2025-01-12 14:47:53 -0300

    adding modalReducer to redux state store

--- store/index.ts
+++ store/index.ts
@@ -2,11 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/store/user/userSlice";
 import toastReducer from "@/store/toast/toastSlice";
+import modalReducer from "@/store/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     toast: toastReducer,
+    modal: modalReducer,
   },
 });
 
