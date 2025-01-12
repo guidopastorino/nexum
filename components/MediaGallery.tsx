68110ddd48f2ff247f2d579b1ba846b9197cbeae
Guido Pastorino <guidopasto05@gmail.com>
2025-01-12 14:49:54 -0300

    improving media gallery for posts

--- components/MediaGallery.tsx
+++ components/MediaGallery.tsx
@@ -1,9 +1,69 @@
+import useModal from "@/hooks/useModal";
 import { MediaFile } from "@/types/types";
 import { isImage } from "@/utils/detectFileType";
+import { useRef, useEffect, useState } from "react";
 
 const MediaGallery = ({ media }: { media: MediaFile[] }) => {
-  const displayMedia = media.slice(0, 4); // Solo mostramos hasta las primeras 4 imágenes
-  const extraCount = media.length - 4; // Cantidad de imágenes extra
+  const { isModalOpen } = useModal('globalModal');
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+  const [isIntersecting, setIsIntersecting] = useState<boolean[]>(new Array(media.length).fill(false));
+
+  const displayMedia = media.slice(0, 4);
+  const extraCount = media.length - 4;
+
+  useEffect(() => {
+    const options = {
+      root: null, // El root es el viewport (pantalla)
+      rootMargin: "0px",
+      threshold: 0.2, // Se considera visible si el 20% del video es visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          // Si el video es visible en el viewport, reproducirlo
+          if (videoRefs.current[index] && !isModalOpen) {
+            videoRefs.current[index]?.play();
+          }
+          setIsIntersecting((prev) => {
+            const newState = [...prev];
+            newState[index] = true;
+            return newState;
+          });
+        } else {
+          // Si el video no está visible, pausarlo
+          if (videoRefs.current[index]) {
+            videoRefs.current[index]?.pause();
+          }
+          setIsIntersecting((prev) => {
+            const newState = [...prev];
+            newState[index] = false;
+            return newState;
+          });
+        }
+      });
+    }, options);
+
+    videoRefs.current.forEach((video) => {
+      if (video) observer.observe(video);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [media.length]);
+
+  // Pausar todos los videos cuando se abra el modal
+  useEffect(() => {
+    if (isModalOpen) {
+      videoRefs.current.forEach((video) => {
+        if (video && !video.paused) {
+          video.pause();
+        }
+      });
+    }
+  }, [isModalOpen]);
 
   if (media.length === 0) return null;
 
@@ -29,8 +89,16 @@ const MediaGallery = ({ media }: { media: MediaFile[] }) => {
             }`}
         >
           {isImage(file)
-            ? <img src={file.url} alt={file.name} className="w-full h-full object-cover rounded-md border borderColor" />
-            : <video src={file.url} controls className="w-full h-full max-h-[550px] object-contain rounded-md border borderColor bg-black"></video>
+            ? <img loading="lazy" src={file.url} alt={file.name} className="w-full h-full object-cover rounded-md border borderColor" />
+            : (
+              <video
+                ref={(el) => { videoRefs.current[i] = el; }}
+                src={file.url}
+                controls
+                className="w-full h-full max-h-[550px] object-contain rounded-md border borderColor bg-black"
+                muted
+              />
+            )
           }
 
           {/* Mostrar el overlay "+n" en la última imagen si hay más de 4 */}
@@ -45,4 +113,4 @@ const MediaGallery = ({ media }: { media: MediaFile[] }) => {
   );
 };
 
-export default MediaGallery
+export default MediaGallery;
